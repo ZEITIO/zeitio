f71ed323c407ca58dab31b8699cefc7ceedbb149
Mark Hagethorn <mhagethorn@gmail.com>
2015-02-09 21:08:59 +0100

    added smooth scroll

--- app/assets/javascripts/zeitio.js
+++ app/assets/javascripts/zeitio.js
@@ -1,27 +1,8 @@
-$(document).ready(function() {
-
-  var owl = $("#owl-testimonials");
-
-  owl.owlCarousel({
-    items : 5, //10 items above 1000px browser width
-    itemsDesktop : [1000,5], //5 items between 1000px and 901px
-    itemsDesktopSmall : [900,3], // betweem 900px and 601px
-    itemsTablet: [600,2], //2 items between 600 and 0
-    itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+$(document).ready(function () {
+  smoothScroll.init({
+    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInOutCubic', // Easing pattern to use
+    updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+    offset: 70, // Integer. How far to offset the scrolling anchor location in pixels
   });
-
-  // Custom Navigation Events
-  $(".next").click(function(){
-    owl.trigger('owl.next');
-  })
-  $(".prev").click(function(){
-    owl.trigger('owl.prev');
-  })
-  $(".play").click(function(){
-    owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-  })
-  $(".stop").click(function(){
-    owl.trigger('owl.stop');
-  })
-
 });
